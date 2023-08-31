f8363c29a3b57391ea98f140fb3ba5fd23cb9d32
HarshitSinghThakur <142519634+HarshitSinghThakur@users.noreply.github.com>
2023-08-31 13:44:47 +0530

    Update 04_comparision.js

--- 01_basics/04_comparision.js
+++ 01_basics/04_comparision.js
@@ -5,17 +5,35 @@
 // console.log(2 != 1);
 
 
-// console.log("2" > 1);
-// console.log("02" > 1);
+//Typescript dont allow two different DT comparision
 
-console.log(null > 0);
-console.log(null == 0);
-console.log(null >= 0);
+// console.log("2" > 1);               => true
+// console.log("02" > 1);              => true
 
-console.log(undefined == 0);
-console.log(undefined > 0);
-console.log(undefined < 0);
 
-// === 
+//Not predicatable result (avoid such type of conversion)
+
+console.log(null > 0);                  =>false
+console.log(null == 0);                 =>false
+console.log(null >= 0);                 =>true
+
+//the reason is that an equality check == and comparisions > ,<, >=, <= work differently.
+//comparisions convert NULL to a number , treating it as 0
+//that's why null >=0 -- true    and null>0 is false;
+ 
+
+
+
+console.log(undefined == 0);     => false
+console.log(undefined > 0);      =>false
+console.log(undefined < 0);      =>false
+
+
+
+// ===     //Strict check --it checks values as well as its data types 
 
 console.log("2" === 2);
+
+
+
+//Summary--- null kabhi 0 toh kabhi NAN mei convert hota hai , so ha ye problem hai language mei, (So avoid such type of conversion ans use clean code.)
