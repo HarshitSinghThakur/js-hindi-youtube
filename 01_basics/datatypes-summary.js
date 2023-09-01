61fcdc422a24fdba1a0bcc2e3aedf746b78d0b54
HarshitSinghThakur <142519634+HarshitSinghThakur@users.noreply.github.com>
2023-09-01 14:48:30 +0530

    Update datatypes-summary.js

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -1,4 +1,4 @@
-//  Primitive
+//  Primitive  //Call by value
 
 //  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
 
@@ -18,20 +18,51 @@ console.log(id === anotherId);
 
 
 
-// Reference (Non primitive)
+// Reference (Non primitive)   //Call by reference
 
 // Array, Objects, Functions
 
-const heros = ["shaktiman", "naagraj", "doga"];
-let myObj = {
+const heros = ["shaktiman", "naagraj", "doga"];      //Array  
+
+let myObj = {                                //Object
     name: "hitesh",
     age: 22,
 }
 
-const myFunction = function(){
+const myFunction = function(){                    //function
     console.log("Hello world");
 }
 
 console.log(typeof anotherId);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+//Stack (primitive) --- call by value 
+// Heap (Non-primitive) ---call by reference
+
+let myYoutubename = "harshit"
+
+let anothername = myYoutubename
+anothername= "singh"
+
+console.log(myYoutubename)        // harshit
+console.log(anothername)            //singh
+
+
+
+//Object 
+
+let userOne = {
+    email: "user@google.com"
+    upi : "user@ubl"
+}
+
+let userTwo = userone
+
+userTwo.email = "harshit@google.com"    //use dot operator to access the object
+
+console.log(userOne.email) //harshit@google.com
+console.log(userTwo.email)    //harshit@google.com
+
