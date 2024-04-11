fad788cd9ea590aaf3d45eaaa28f9ec774775a2e
HarshitSinghThakur <142519634+HarshitSinghThakur@users.noreply.github.com>
2024-04-11 20:16:02 +0530

    Update Object.js

--- 10_classes_and_oop/Object.js
+++ 10_classes_and_oop/Object.js
@@ -9,6 +9,9 @@ console.log(multipleBy5(5));
 console.log(multipleBy5.power);
 console.log(multipleBy5.prototype);
 
+
+// this -> gives current context
+
 function createUser(username, score){
     this.username = username
     this.score = score
